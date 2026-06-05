0222c81b1220a45532a70fb50b6c3b7ece49a440
ogasurfproject-jpg <oga.surf.project@gmail.com>
2026-06-05 18:18:28 +0900

    Add files via upload

---
+++ hacker/sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'ehn-shell-v1';
+const SHELL = ['/hacker/', '/hacker/index.html', '/hacker/icon-192.png', '/hacker/icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)).catch(() => {}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+  if (url.origin !== location.origin) return;
+  if (url.pathname.startsWith('/hacker/') && (url.pathname === '/hacker/' || url.pathname.endsWith('.html'))) {
+    e.respondWith(
+      fetch(req).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(req, copy)).catch(() => {});
+        return res;
+      }).catch(() => caches.match(req).then((r) => r || caches.match('/hacker/index.html')))
+    );
+    return;
+  }
+  e.respondWith(caches.match(req).then((r) => r || fetch(req)));
+});
